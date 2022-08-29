cfcea47af59616f0b1a071150754616b00b6f6bd
AlexandreJareck <alexandrejareck@gmail.com>
2022-08-29 19:46:34 -0300

    cannot

---
+++ src/DevIO.Business/DevIO.Business/Models/Provider.cs
@@ -0,0 +1,13 @@
+﻿namespace DevIO.Business.Models;
+
+public class Provider : Entity
+{
+    public string Name { get; set; }
+    public string Document { get; set; }
+    public bool Active { get; set; }
+
+    public TypeProvider TypeProvider { get; set; }
+    public Address Addreess { get; set; }
+
+    public IEnumerable<Product> Products { get; set; }
+}
---
+++ src/DevIO.Business/DevIO.Business/Models/TypeProvider.cs
@@ -0,0 +1,7 @@
+﻿namespace DevIO.Business.Models;
+
+public enum TypeProvider
+{
+    LegalPerson = 1,
+    PhysicalPerson
+}
