f1641964f43275d820565fd89035ff1568b9f4bc
AlexandreJareck <alexandrejareck@gmail.com>
2022-08-29 18:58:29 -0300

    feat: created models in project business

---
+++ src/DevIO.Business/DevIO.Business/Models/Address.cs
@@ -0,0 +1,16 @@
+﻿namespace DevIO.Business.Models;
+
+public class Address
+{
+    public Guid ProviderId { get; set; }
+    public string Street { get; set; }
+    public string Number { get; set; }
+    public string Complement { get; set; }
+    public string Zipcode { get; set; }
+    public string Neighborhood { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+
+    /* EF Relation */
+    public Provider Provider { get; set; }
+}
