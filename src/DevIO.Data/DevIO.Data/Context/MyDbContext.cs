6587afdcf5a9ac4f9ad5c4b8f05b1c3a967064b7
AlexandreJareck <alexandrejareck@gmail.com>
2022-08-29 19:04:43 -0300

    chore: install entity framework

---
+++ src/DevIO.Data/DevIO.Data/Context/MyDbContext.cs
@@ -0,0 +1,16 @@
+﻿using DevIO.Business.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevIO.Data.Context;
+
+public class MyDbContext : DbContext
+{
+    public MyDbContext(DbContextOptions options) : base(options)
+    {
+
+    }
+
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Address> Addresses { get; set; }
+    public DbSet<Provider> Providers { get; set; }
+}
