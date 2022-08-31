9f30a07981bccb2a0b05d32886fbac51855423ff
AlexandreJareck <alexandrejareck@gmail.com>
2022-08-31 20:10:46 -0300

    feat: add validation business in provider with fluent validation

--- src/DevIO.Business/DevIO.Business/Models/Provider.cs
+++ src/DevIO.Business/DevIO.Business/Models/Provider.cs
@@ -1,4 +1,7 @@
-﻿namespace DevIO.Business.Models;
+﻿using DevIO.Business.Models.Validations;
+using FluentValidation;
+
+namespace DevIO.Business.Models;
 
 public class Provider : Entity
 {
@@ -10,4 +13,31 @@ public class Provider : Entity
     public Address Addreess { get; set; }
 
     public IEnumerable<Product> Products { get; set; }
+}
+
+public class ProviderValidation : AbstractValidator<Provider>
+{
+    public ProviderValidation()
+    {
+        RuleFor(f => f.Name)
+                .NotEmpty().WithMessage("O campo {PropertyName} precisa ser fornecido")
+                .Length(2, 100)
+                .WithMessage("O campo {PropertyName} precisa ter entre {MinLength} e {MaxLength} caracteres");
+
+        When(f => f.TypeProvider == TypeProvider.PhysicalPerson, () =>
+        {
+            RuleFor(f => f.Document.Length).Equal(CpfValidator.DocumentLength)
+                .WithMessage("O campo Documento precisa ter {ComparisonValue} caracteres e foi fornecido {PropertyValue}.");
+            RuleFor(f => CpfValidator.Validation(f.Document)).Equal(true)
+                .WithMessage("O documento fornecido é inválido.");
+        });
+
+        When(f => f.TypeProvider == TypeProvider.LegalPerson, () =>
+        {
+            RuleFor(f => f.Document.Length).Equal(CnpjValidator.Length)
+                .WithMessage("O campo Documento precisa ter {ComparisonValue} caracteres e foi fornecido {PropertyValue}.");
+            RuleFor(f => CnpjValidator.Validation(f.Document)).Equal(true)
+                .WithMessage("O documento fornecido é inválido.");
+        });
+    }
 }
