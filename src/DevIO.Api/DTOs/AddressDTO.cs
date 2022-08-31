56a8003ef300ff4b2db17de7e36cf1c41daab9f4
AlexandreJareck <alexandrejareck@gmail.com>
2022-08-31 01:40:04 -0300

    feat: created DTO's

---
+++ src/DevIO.Api/DTOs/AddressDTO.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DevIO.Api.DTOs
+{
+    public class AddressDTO
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(200, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracateres", MinimumLength = 2)]
+        public string Street { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(50, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracateres", MinimumLength = 1)]
+        public string Number { get; set; }
+
+        public string Complement { get; set; }
+        public string Zipcode { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracateres", MinimumLength = 2)]
+        public string Neighborhood { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracateres", MinimumLength = 2)]
+        public string City { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracateres", MinimumLength = 2)]
+        public string State { get; set; }
+
+        public Guid ProviderId { get; set; }
+    }
+}
---
+++ src/DevIO.Api/DTOs/ProviderDTO.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DevIO.Api.DTOs;
+
+public class ProviderDTO
+{
+    [Key]
+    public Guid Id { get; set; }
+
+    [Required(ErrorMessage = "O campo {0} é obrigatório")]
+    [StringLength(14, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracateres", MinimumLength = 2)]
+    public string Name { get; set; }
+
+    [Required(ErrorMessage = "O campo {0} é obrigatório")]
+    [StringLength(14, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracateres", MinimumLength = 11)]
+    public string Document { get; set; }
+    public int TypeProvider { get; set; }
+    public AddressDTO Address { get; set; }
+    public bool Active { get; set; }
+
+    public IEnumerable<ProductDTO> Products { get; set; }
+}
