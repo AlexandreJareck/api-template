e7ce970030ba42b929e92e67de67d0f2a7c2ed73
AlexandreJareck <alexandrejareck@gmail.com>
2022-08-31 00:55:50 -0300

    feat: add Productrepositotory

---
+++ src/DevIO.Data/DevIO.Data/Repository/ProductRepository.cs
@@ -0,0 +1,30 @@
+﻿using DevIO.Business.Interfaces;
+using DevIO.Business.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevIO.Data.Repository;
+
+public class ProductRepository : Repository<Product>, IProductRepository
+{
+    public async Task<Product> GetProductProvider(Guid id)
+    {
+        return await Db.Products
+            .AsNoTracking()
+            .Include(p => p.Provider)
+            .FirstOrDefaultAsync(p => p.Id == id) 
+            ?? new Product();
+    }
+
+    public async Task<IEnumerable<Product>> GetProductProviders()
+    {
+        return await Db.Products
+            .AsNoTracking()
+            .Include(p => p.Provider).OrderBy(p => p.Name)
+            .ToListAsync();
+    }
+
+    public async Task<IEnumerable<Product>> GetProductsByProvider(Guid providerId)
+    {
+        return await Get(p => p.ProviderId == providerId);
+    }
+}
