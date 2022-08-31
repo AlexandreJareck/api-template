0c7c4a1de3b5c85a4be036f046cdd81b9eb8a228
AlexandreJareck <alexandrejareck@gmail.com>
2022-08-31 01:14:49 -0300

    feat: add AddressRepository

---
+++ src/DevIO.Data/DevIO.Data/Repository/AddressRepository.cs
@@ -0,0 +1,21 @@
+﻿using DevIO.Business.Interfaces;
+using DevIO.Business.Models;
+using DevIO.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevIO.Data.Repository;
+
+public class AddressRepository : Repository<Address>, IAddressRepository
+{
+    public AddressRepository(MyDbContext context) : base(context)
+    {
+
+    }
+    public async Task<Address> GetAddressByProvider(Guid providerId)
+    {
+        return await Db.Addresses
+            .AsNoTracking()
+            .FirstOrDefaultAsync(p => p.ProviderId == providerId)
+            ?? new Address();
+    }
+}
