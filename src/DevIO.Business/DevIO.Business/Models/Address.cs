500d84757374e2b0087bed657144db929814d159
AlexandreJareck <alexandrejareck@gmail.com>
2022-09-01 00:10:45 -0300

    feat: add validation in product and address

--- src/DevIO.Business/DevIO.Business/Models/Address.cs
+++ src/DevIO.Business/DevIO.Business/Models/Address.cs
@@ -1,4 +1,6 @@
-﻿namespace DevIO.Business.Models;
+﻿using FluentValidation;
+
+namespace DevIO.Business.Models;
 
 public class Address : Entity
 {
@@ -13,4 +15,35 @@ public class Address : Entity
 
     /* EF Relation */
     public Provider Provider { get; set; }
+}
+
+
+public class AddressValidation : AbstractValidator<Address>
+{
+    public AddressValidation()
+    {
+        RuleFor(c => c.Street)
+                .NotEmpty().WithMessage("O campo {PropertyName} precisa ser fornecido")
+                .Length(2, 200).WithMessage("O campo {PropertyName} precisa ter entre {MinLength} e {MaxLength} caracteres");
+
+        RuleFor(c => c.Neighborhood)
+            .NotEmpty().WithMessage("O campo {PropertyName} precisa ser fornecido")
+            .Length(2, 100).WithMessage("O campo {PropertyName} precisa ter entre {MinLength} e {MaxLength} caracteres");
+
+        RuleFor(c => c.Zipcode)
+            .NotEmpty().WithMessage("O campo {PropertyName} precisa ser fornecido")
+            .Length(8).WithMessage("O campo {PropertyName} precisa ter {MaxLength} caracteres");
+
+        RuleFor(c => c.City)
+            .NotEmpty().WithMessage("A campo {PropertyName} precisa ser fornecida")
+            .Length(2, 100).WithMessage("O campo {PropertyName} precisa ter entre {MinLength} e {MaxLength} caracteres");
+
+        RuleFor(c => c.State)
+            .NotEmpty().WithMessage("O campo {PropertyName} precisa ser fornecido")
+            .Length(2, 50).WithMessage("O campo {PropertyName} precisa ter entre {MinLength} e {MaxLength} caracteres");
+
+        RuleFor(c => c.Number)
+            .NotEmpty().WithMessage("O campo {PropertyName} precisa ser fornecido")
+            .Length(1, 50).WithMessage("O campo {PropertyName} precisa ter entre {MinLength} e {MaxLength} caracteres");
+    }
 }
--- src/DevIO.Business/DevIO.Business/Models/Product.cs
+++ src/DevIO.Business/DevIO.Business/Models/Product.cs
@@ -1,4 +1,6 @@
-﻿namespace DevIO.Business.Models;
+﻿using FluentValidation;
+
+namespace DevIO.Business.Models;
 
 public class Product : Entity
 {
@@ -12,4 +14,22 @@ public class Product : Entity
     public bool Active { get; set; }
 
     public Provider Provider { get; set; }
+}
+
+
+public class ProductValidation : AbstractValidator<Product>
+{
+    public ProductValidation()
+    {
+        RuleFor(c => c.Name)
+                .NotEmpty().WithMessage("O campo {PropertyName} precisa ser fornecido")
+                .Length(2, 200).WithMessage("O campo {PropertyName} precisa ter entre {MinLength} e {MaxLength} caracteres");
+
+        RuleFor(c => c.Description)
+            .NotEmpty().WithMessage("O campo {PropertyName} precisa ser fornecido")
+            .Length(2, 1000).WithMessage("O campo {PropertyName} precisa ter entre {MinLength} e {MaxLength} caracteres");
+
+        RuleFor(c => c.Value)
+            .GreaterThan(0).WithMessage("O campo {PropertyName} precisa ser maior que {ComparisonValue}");
+    }
 }
