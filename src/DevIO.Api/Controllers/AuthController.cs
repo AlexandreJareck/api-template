04253459a4b93891c5f9888ccac9466acac56b28
AlexandreJareck <alexandrejareck@gmail.com>
2022-09-08 20:14:23 -0300

    update(AuthController):  add login and register

--- src/DevIO.Api/Controllers/AuthController.cs
+++ src/DevIO.Api/Controllers/AuthController.cs
@@ -1,11 +1,76 @@
-﻿using DevIO.Business.Interfaces;
+﻿using DevIO.Api.DTOs;
+using DevIO.Business.Interfaces;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DevIO.Api.Controllers
 {
+    [Route("api")]
     public class AuthController : MainController
     {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AuthController(INotifier notifier,
+                              SignInManager<IdentityUser> signInManager,
+                              UserManager<IdentityUser> userManager) : base(notifier)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
         public AuthController(INotifier notifier) : base(notifier)
         {
+        }
+
+        [HttpPost("/nova-conta")]
+        public async Task<ActionResult> Register(RegisterUserDTO registerUser)
+        {
+            if (!ModelState.IsValid) return CustomResponse(ModelState);
+
+            var user = new IdentityUser
+            {
+                UserName = registerUser.Email,
+                Email = registerUser.Email,
+                EmailConfirmed = true
+            };
+
+            var result = await _userManager.CreateAsync(user, registerUser.Password);
+
+            if (result.Succeeded)
+            {
+                await _signInManager.SignInAsync(user, false);
+                return CustomResponse(registerUser);
+            }
+
+            foreach (var error in result.Errors)
+            {
+                NotifyError(error.Description);
+            }
+
+            return CustomResponse(registerUser);
+        }
+
+        [HttpPost("/entrar")]
+        public async Task<ActionResult> Login(LoginUserDTO loginUser)
+        {
+            if (!ModelState.IsValid) return CustomResponse(ModelState);
+
+            var result = await _signInManager.PasswordSignInAsync(loginUser.Email, loginUser.Password, false, true);
+
+            if (result.Succeeded)
+            {
+                return CustomResponse(loginUser);
+            }
+
+            if (result.IsLockedOut)
+            {
+                NotifyError("Usuário temporariamente bloquado por tentativas inválidas");
+                return CustomResponse(loginUser);
+            }
+
+            NotifyError("Usuário ou Senha incorretos");
+            return CustomResponse(loginUser);
         }
     }
 }
