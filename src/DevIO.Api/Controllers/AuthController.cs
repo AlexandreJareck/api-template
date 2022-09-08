025321bb2d9f2d8aba3d48763ec30ac467a28a10
AlexandreJareck <alexandrejareck@gmail.com>
2022-09-07 22:24:18 -0300

    feat: add class auth controller

---
+++ src/DevIO.Api/Controllers/AuthController.cs
@@ -0,0 +1,11 @@
+﻿using DevIO.Business.Interfaces;
+
+namespace DevIO.Api.Controllers
+{
+    public class AuthController : MainController
+    {
+        public AuthController(INotifier notifier) : base(notifier)
+        {
+        }
+    }
+}
