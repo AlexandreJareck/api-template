290f3c2eeac76a262b352bfeac66346877c00651
AlexandreJareck <alexandrejareck@gmail.com>
2022-09-11 13:24:31 -0300

    feat: crated asp net user with implementation IUser

---
+++ src/DevIO.Api/Extensions/AspNetUser.cs
@@ -0,0 +1,60 @@
+﻿using DevIO.Business.Interfaces;
+using System.Security.Claims;
+
+namespace DevIO.Api.Extensions;
+
+public class AspNetUser : IUser
+{
+    private readonly IHttpContextAccessor _accessor;
+
+    public AspNetUser(IHttpContextAccessor accessor)
+    {
+        _accessor = accessor;
+    }
+
+    public string Name => _accessor.HttpContext.User.Identity.Name;
+    public Guid GetUserId()
+    {
+        return IsAuthrenticated() ? Guid.Parse(_accessor.HttpContext.User.GetUserId()) : Guid.NewGuid();
+    }
+    public string GetUserEmail()
+    {
+        return IsAuthrenticated() ? _accessor.HttpContext.User.GetUserEmail() : "";
+    }
+    public bool IsAuthrenticated()
+    {
+        return _accessor.HttpContext.User.Identity.IsAuthenticated;
+    }
+    public IEnumerable<Claim> GetClaimsIdentity()
+    {
+        return _accessor.HttpContext.User.Claims;
+    }
+
+    public bool IsInRole(string role)
+    {
+        return _accessor.HttpContext.User.IsInRole(role);
+    }
+}
+
+public static class ClaimsPrincipalExtensions
+{
+    public static string GetUserId(this ClaimsPrincipal principal)
+    {
+        if (principal == null)
+            throw new ArgumentNullException(nameof(principal));
+
+        var claim = principal.FindFirst(ClaimTypes.NameIdentifier);
+        return claim?.Value!;
+    }
+
+    public static string GetUserEmail(this ClaimsPrincipal principal)
+    {
+        if (principal == null)
+        {
+            throw new ArgumentException(nameof(principal));
+        }
+
+        var claim = principal.FindFirst(ClaimTypes.Email);
+        return claim?.Value!;
+    }
+}
